605fcfe2348f18811bf44326fbd80b958cdbbbc4
fiveflower0130 <fiveflower0130@gmail.com>
2021-08-19 21:56:44 +0800

    update send message

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+from linebot.models.messages import StickerMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('R5xWYeC0pvKPbizw+L4Ij8qgTwm583uCrdWVPnAucDpVd4wK47D2eV5KHvIKVafue5wQZ14CU27bhJoLq7pZ0S3GRZN0jbYgjTDwk4sWHL4avWOS7KP7VxqL79JiyOyPSj+1i8Sc68R8K04pT3gagAdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    'R5xWYeC0pvKPbizw+L4Ij8qgTwm583uCrdWVPnAucDpVd4wK47D2eV5KHvIKVafue5wQZ14CU27bhJoLq7pZ0S3GRZN0jbYgjTDwk4sWHL4avWOS7KP7VxqL79JiyOyPSj+1i8Sc68R8K04pT3gagAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('64a2f536f82c74d58b59120a151100f7')
 
 
@@ -38,9 +40,23 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    rmsg = '哈囉，聽不懂你在說甚麼~'
+
+    if "圖" or "貼圖" in msg:
+        sticker_msg = StickerMessage(
+            package_id="2",
+            sticker_id="23",
+        )
+        line_bot_api.reply_message(
+            event.reply_token,
+            StickerMessage)
+        return
+
+    if msg in ["hi", "Hi", "hello", "Hello"]:
+        rmsg = "嗨，我是賈斯伯!"
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="Hi Hi I'm Jasper!"))
+        TextSendMessage(text=rmsg))
 
 
 if __name__ == "__main__":
